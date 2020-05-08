de6eeb2ced3fb2a75e1291f68edb32de69abaa6a
Pragadees <prgwar@gmail.com>
2020-05-09 04:41:48 +0530

    first

--- test.sh
+++ test.sh
@@ -1,3 +1,12 @@
-echo "hi"
-ls
-flutter doctor
+
+Merchants=('demoMerchant' "merchant1")
+buildApk(){
+  echo "building...$1"
+#  flutter build apk --flavor $1
+}
+for merchant in $Merchants
+do
+   buildApk $merchant & done
+wait
+echo "building complete"
+exit 0
